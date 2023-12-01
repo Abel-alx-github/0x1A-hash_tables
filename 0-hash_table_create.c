2a488123565c77e01a807dac51a432c66a8d4bea
abel-alx-github <abeltibeso323@gmail.com>
2023-12-01 03:19:45 -0800

    task 0

--- 0-hash_table_create.c
+++ 0-hash_table_create.c
@@ -15,10 +15,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = (hash_node_t **)calloc(new_table->size, sizeof(hash_node_t*));
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 	{
-		free(new_table);
 		return (NULL);
 	}
 	i = 0;
